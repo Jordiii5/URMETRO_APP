cf7c426f61b42367e33bfd06c1b7c99b1c44dfb8
Joel <joelgarciag04@gmail.com>
2024-04-02 16:49:10 +0200

    Afegir Binding a cada Fragment

--- app/src/main/java/com/example/urmetro/ModulMovilitatFragment.kt
+++ app/src/main/java/com/example/urmetro/ModulMovilitatFragment.kt
@@ -5,16 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.urmetro.databinding.FragmentRegistreBinding
+import com.example.urmetro.databinding.FragmentModulMovilitatBinding
 
-class RegistreFragment : Fragment() {
-    lateinit var binding: FragmentRegistreBinding
+class ModulMovilitatFragment : Fragment() {
+    lateinit var binding: FragmentModulMovilitatBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentRegistreBinding.inflate(layoutInflater)
+        binding = FragmentModulMovilitatBinding.inflate(layoutInflater)
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {

5c2cf5ef661b9ece2e562b23cf6dfbce0f1ee9e3
Joel <joelgarciag04@gmail.com>
2024-04-08 16:21:59 +0200

    Diseny del menu y plantejament de la navegació

--- app/src/main/java/com/example/urmetro/MenuFragment.kt
+++ app/src/main/java/com/example/urmetro/MenuFragment.kt
@@ -1,10 +1,12 @@
 package com.example.urmetro
 
 import android.os.Bundle
+import android.text.Layout.Directions
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.urmetro.databinding.FragmentMenuBinding
 
 class MenuFragment : Fragment() {
@@ -19,6 +21,19 @@ class MenuFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.entreteniment.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_modulEntretenimentFragment)
+        }
+        binding.sanitat.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_modulSanitatFragment)
+        }
+        binding.socialitzacio.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_modulSocialitzacioFragment)
+        }
+        binding.movilitat.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_modulMovilitatFragment)
+        }
     }
 
 }

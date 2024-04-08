e3e78efbcd59e0984cd1dc5e2d6c9143362f3134
Joel <joelgarciag04@gmail.com>
2024-04-08 19:21:44 +0200

    Capçaleres de socialització y navegació

--- app/src/main/java/com/example/urmetro/LlistaContactesFragment.kt
+++ app/src/main/java/com/example/urmetro/LlistaContactesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.urmetro.databinding.FragmentLlistaContactesBinding
 
 class LlistaContactesFragment : Fragment() {
@@ -19,6 +20,10 @@ class LlistaContactesFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.arrowBack.setOnClickListener {
+            findNavController().navigate(R.id.action_llistaContactesFragment_to_modulSocialitzacioFragment)
+        }
     }
 
 }

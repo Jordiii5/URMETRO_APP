cef04e1d16ee66f46aa73f3b69901e4c39625592
Joel Montalvan <joel.montalvan.7e6@itb.cat>
2024-04-10 17:47:28 +0200

    Intento de conexion login/register API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
 }
 
 android {
@@ -50,4 +51,34 @@ dependencies {
 
     implementation ("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation ("androidx.navigation:navigation-ui-ktx:2.5.3")
+
+    implementation ("com.airbnb.android:lottie:3.4.0")
+
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    kapt ("com.github.bumptech.glide:compiler:4.12.0")
+
+    implementation ("androidx.recyclerview:recyclerview:1.3.0")
+    implementation ("androidx.recyclerview:recyclerview-selection:1.1.0")
+
+    //ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0-rc01")
+    //LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0-rc01")
+    //by viewModels extension for Activity
+    implementation("androidx.activity:activity-ktx:1.3.1")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
+    implementation ("io.github.rburgst:okhttp-digest:3.0.1")
+
+
+    implementation ("androidx.camera:camera-core:1.1.0-beta02")
+    implementation ("androidx.camera:camera-camera2:1.1.0-beta02")
+    implementation ("androidx.camera:camera-lifecycle:1.1.0-beta02")
+    implementation ("androidx.camera:camera-view:1.1.0-beta02")
+    implementation ("androidx.camera:camera-extensions:1.1.0-beta02")
 }

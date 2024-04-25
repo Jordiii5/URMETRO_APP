2ea7f12ad168b5e8717721587ff3899baece0cae
genis.fernandez.7e5 <genis.fernandez.7e5@itb.cat>
2024-04-25 16:47:59 +0200

    fragment_llista_contactes.xml

--- app/src/main/java/com/example/urmetro/MenuFragment.kt
+++ app/src/main/java/com/example/urmetro/MenuFragment.kt
@@ -46,9 +46,7 @@ class MenuFragment : Fragment() {
         binding.usuari.setOnClickListener {
             findNavController().navigate(R.id.action_menuFragment_to_iniciarSessioFragment)
         }
-        binding.perfil.setOnClickListener {
-            findNavController().navigate(R.id.action_menuFragment_to_perfilFragment)
-        }
+
     }
 
 }

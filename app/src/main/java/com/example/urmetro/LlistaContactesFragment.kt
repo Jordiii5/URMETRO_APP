b2fba02037876eb8a0e459a1ede60d345e61dcda
genis.fernandez.7e5 <genis.fernandez.7e5@itb.cat>
2024-04-26 17:35:31 +0200

    fragment_llista_contactes.xml

--- app/src/main/java/com/example/urmetro/LlistaContactesFragment.kt
+++ app/src/main/java/com/example/urmetro/LlistaContactesFragment.kt
@@ -1,5 +1,6 @@
 package com.example.urmetro
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -23,6 +24,13 @@ class LlistaContactesFragment : Fragment() {
 
         binding.arrowBack.setOnClickListener {
             findNavController().navigate(R.id.action_llistaContactesFragment_to_modulSocialitzacioFragment)
+        }
+        binding.afegirContacteButton.setOnClickListener{
+            val builder = AlertDialog.Builder( this.context)
+            val view = layoutInflater.inflate(R.layout.dialog_contactes,null)
+            builder.setView(view)
+            val dialog = builder.create()
+            dialog.show()
         }
     }
 

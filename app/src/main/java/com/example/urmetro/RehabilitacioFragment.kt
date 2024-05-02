c20fe925b09049900a821e24ad97fb68aab5c926
Joel <joelgarciag04@gmail.com>
2024-05-02 16:58:17 +0200

    Dialog amb codi de seguretat

--- app/src/main/java/com/example/urmetro/RehabilitacioFragment.kt
+++ app/src/main/java/com/example/urmetro/RehabilitacioFragment.kt
@@ -1,10 +1,13 @@
 package com.example.urmetro
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.navigation.fragment.findNavController
 import com.example.urmetro.databinding.FragmentRehabilitacioBinding
 
@@ -24,6 +27,26 @@ class RehabilitacioFragment : Fragment() {
         binding.arrowBack.setOnClickListener {
             findNavController().navigate(R.id.action_rehabilitacioFragment_to_modulSanitatFragment)
         }
+        binding.programRobot.setOnClickListener {
+            showChangePasswordDialog()
+        }
     }
+    private fun showChangePasswordDialog() {
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.dialog_programacio)
 
+        val codiSeg="1234"
+
+        val botonNo = dialog.findViewById<Button>(R.id.boto_no)
+        val botonSi = dialog.findViewById<Button>(R.id.boto_si)
+        val codi = dialog.findViewById<EditText>(R.id.codi)
+        botonNo.setOnClickListener {
+            dialog.hide()
+        }
+        botonSi.setOnClickListener {
+            if(codi.text.toString() == codiSeg) findNavController().navigate(R.id.action_rehabilitacioFragment_to_programarRobotFragment)
+            dialog.hide()
+        }
+        dialog.show()
+    }
 }

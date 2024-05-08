09cca84c7bc8052c2fdcc4e0a6c455eae22c7a08
genis.fernandez.7e5 <genis.fernandez.7e5@itb.cat>
2024-05-08 19:07:12 +0200

    fragment_cites.xml

--- app/src/main/java/com/example/urmetro/AfegirContacteFragment.kt
+++ app/src/main/java/com/example/urmetro/AfegirContacteFragment.kt
@@ -1,29 +1,29 @@
-package com.example.urmetro
-
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.urmetro.databinding.FragmentAfegirContacteBinding
-
-class AfegirContacteFragment : Fragment() {
-    lateinit var binding: FragmentAfegirContacteBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentAfegirContacteBinding.inflate(layoutInflater)
-        return binding.root
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.arrowBack.setOnClickListener {
-            findNavController().navigate(R.id.action_afegirContacteFragment_to_llistaContactesFragment)
-        }
-    }
-
-}
+//package com.example.urmetro
+//
+//import android.os.Bundle
+//import androidx.fragment.app.Fragment
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.navigation.fragment.findNavController
+//import com.example.urmetro.databinding.FragmentAfegirContacteBinding
+//
+//class AfegirContacteFragment : Fragment() {
+//    lateinit var binding: FragmentAfegirContacteBinding
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        binding = FragmentAfegirContacteBinding.inflate(layoutInflater)
+//        return binding.root
+//    }
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        binding.arrowBack.setOnClickListener {
+//            findNavController().navigate(R.id.action_afegirContacteFragment_to_llistaContactesFragment)
+//        }
+//    }
+//
+//}

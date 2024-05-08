68cac37a8cf0f3f0f7c2bc3a660aa851b85090b2
Joel <joelgarciag04@gmail.com>
2024-05-08 18:23:25 +0200

    Reproduir Videos

--- app/src/main/java/com/example/urmetro/RehabilitacioFragment.kt
+++ app/src/main/java/com/example/urmetro/RehabilitacioFragment.kt
@@ -1,11 +1,15 @@
 package com.example.urmetro
 
+import android.annotation.SuppressLint
 import android.app.Dialog
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -22,6 +26,7 @@ class RehabilitacioFragment : Fragment() {
         binding = FragmentRehabilitacioBinding.inflate(layoutInflater)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -31,19 +36,56 @@ class RehabilitacioFragment : Fragment() {
         binding.programRobot.setOnClickListener {
             showChangePasswordDialog()
         }
+        reprodicirVideos()
+
+
+
     }
+
+    /**
+     *@author Joel Garcia
+     *
+     * Aquesta funció agafa els tres webViews de la pantalla y els asigna un video de youtube
+     */
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun reprodicirVideos() {
+        val video1 = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/7IlAEVcAOlA?si=pS2TUcZoy1dplJ7X\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>"
+        binding.video1.loadData(video1,"text/html","utf-8")
+        binding.video1.settings.javaScriptEnabled=true
+        binding.video1.webChromeClient= WebChromeClient()
+
+        val video2 = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/OP9w1b1-B4g?si=BNUp2zNb_GGUaBv8\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>"
+        binding.video2.loadData(video2,"text/html","utf-8")
+        binding.video2.settings.javaScriptEnabled=true
+        binding.video2.webChromeClient= WebChromeClient()
+
+        val video3= "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/-QYqhmQj_ZI?si=On24NnKABttRxfiC\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>"
+        binding.video3.loadData(video3,"text/html","utf-8")
+        binding.video3.settings.javaScriptEnabled=true
+        binding.video3.webChromeClient= WebChromeClient()
+    }
+
+
+    /**
+     * @author Joel Garcia
+     *
+     * Aquesta funció mostra un dialeg que ofereix la opció de anar a la personalització de programes
+     */
     private fun showChangePasswordDialog() {
         val dialog = Dialog(requireContext())
         dialog.setContentView(R.layout.dialog_programacio)
 
         val codiSeg="1234"
 
+        //Fixem els botons a variables
         val botonNo = dialog.findViewById<Button>(R.id.boto_no)
         val botonSi = dialog.findViewById<Button>(R.id.boto_si)
         val codi = dialog.findViewById<EditText>(R.id.codi)
+        //Si pulsem aquest botó es tancatá el dialeg
         botonNo.setOnClickListener {
             dialog.hide()
         }
+        //Aques botó comproba si el cosi de seguretat es correcte, si ho es, fa la navegació
         botonSi.setOnClickListener {
             if(codi.text.toString() == codiSeg) {
                 findNavController().navigate(R.id.action_rehabilitacioFragment_to_programarRobotFragment)
